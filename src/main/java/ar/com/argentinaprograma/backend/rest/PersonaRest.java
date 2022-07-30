400b8341befa2e7e4719238cdf3ccbac4222dc47
emegea <martosproject@gmail.com>
2022-07-30 13:13:53 -0300

    back

--- src/main/java/ar/com/argentinaprograma/backend/rest/PersonaRest.java
+++ src/main/java/ar/com/argentinaprograma/backend/rest/PersonaRest.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "https://ap-frontend-emegea.web.app/")
+@CrossOrigin (origins = "https://ap-frontend-emegea.web.app")
 @RequestMapping("/api/persona/")
 public class PersonaRest {
 
